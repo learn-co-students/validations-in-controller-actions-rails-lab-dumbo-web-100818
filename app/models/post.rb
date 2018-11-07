e02bab7e351a16771b743228e16f01cba75ab92e
achadha2 <andy.chadha2@gmail.com>
2018-11-07 10:12:42 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  # validates_inclusion_of :category, :in => ["Fiction", "Non-Fiction"], :allow_nil => false
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :content, length: {minimum: 100}
 end
